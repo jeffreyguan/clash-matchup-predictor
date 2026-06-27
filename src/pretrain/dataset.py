594620f92a82648330d77780f448dabf205ff69c
JEFFREY GUAN <jeffreyguan0710@gmail.com>
2026-06-27 15:29:29 -0400

    pretrain dataset done

--- src/pretrain/dataset.py
+++ src/pretrain/dataset.py
@@ -3,5 +3,44 @@ import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 
 class PretrainDataset(Dataset):
-    def __init__(self, decks, n_mask=1, mask_idx=0, numcards=122, train=True):
+    def __init__(self, data, n_mask=1, mask_idx=0, ignore_idx = -100, numcards=122, train=True):
+        self.decks = torch.cat([
+            torch.tensor([item[:8] for item in data], dtype=torch.long),    # team decks  -> [N, 8]
+            torch.tensor([item[8:16] for item in data], dtype=torch.long),  # opp decks   -> [N, 8]
+        ], dim=0)                                                           # stacked     -> [2N, 8]
+        self.n_mask = n_mask
+        self.mask_idx = mask_idx
+        self.ignore_idx = ignore_idx
+        self.numcards = numcards
+        self.train = train
+
+        if not self.train:
+            g = torch.Generator().manual_seed(0)          # seed -> reproducible val masks
+            self.examples = [self._maskdeck(deck, generator=g) for deck in self.decks]
+
+
+    def __len__(self):
+        return len(self.decks)
+    
+    def __getitem__(self, idx):
+        if self.train:
+            return self._maskdeck(self.decks[idx])    
+        return self.examples[idx]                     
+
+
+    def _maskdeck(self, deck, generator=None):
+        input_ids = deck.clone()
+        labels = torch.full_like(deck, self.ignore_idx)
+        idx = torch.randperm(8, generator=generator)[:self.n_mask]
+        labels[idx] = deck[idx]
         
+        for i in idx:
+            r = torch.rand(1, generator=generator).item()
+            if r < 0.8:
+                input_ids[i] = self.mask_idx                                              # 80% mask
+            elif r < 0.9:
+                input_ids[i] = torch.randint(1, self.numcards, (1,), generator=generator) # 10% random card
+            else:
+                input_ids[i] = deck[i]                                                    
+
+        return (input_ids, labels)
